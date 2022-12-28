ba523be39a8b57f2a51c458a2a52b759ead5c2e1
Madhusudhan Reddy <madhuman1012@gmail.com>
2022-12-28 23:36:25 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
+
+# Create your views here.
+
+# Production API key
+# api_key = 'k3Mc19hGid7dpm7p5cGNbgl2Ocy15TV4q6GtuGb1'
+
+# Development API key
+api_key = 'DEMO_KEY'
+
+
+def index(request):
+
+    response = requests.get(
+        'https://api.nasa.gov/planetary/apod?api_key=' + api_key)
+
+    if response.status_code == 200:
+        data = response.json()
+        return render(request, 'blog/index.html', {'data': data})
+    else:
+        return HttpResponse("Error")
